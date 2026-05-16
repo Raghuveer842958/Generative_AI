cf6a7c5f9a7f902cd630d181d8c965cee6b9ee94
Raghuveer842958 <raghuveerchauhan@gmail.com>
2026-05-16 16:56:17 +0530

    create AI chatbot using LangChain and Hugging Face

---
+++ Project/chatbot.py
@@ -0,0 +1,32 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate, load_prompt
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+
+load_dotenv()
+
+model = HuggingFaceEndpoint(
+    repo_id = "deepseek-ai/DeepSeek-V3",
+    temperature = 0.5,
+    task = "text-generation",
+)
+
+llm = ChatHuggingFace(llm=model)
+
+chat_history = [
+    SystemMessage(content="you are a helpfull assistant"),
+]
+
+while True:
+    user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input.lower() == "exit":
+        print("Exiting the chat. Goodbye!")
+        break
+
+    result = llm.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ", result.content)
+
+
+print("Chat history: ", chat_history)
